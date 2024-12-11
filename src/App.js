16cba4cde1be17f12feeb1e4ca669098343b85c7
FabrizioCano <fabriziocano7@gmail.com>
2024-12-11 19:35:34 -0300

    Search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{useState,useEffect} from "react";
 import ImageCard from './components/ImageCard';
+import SearchBar from "./components/SearchBar";
+
 
 function App() {
   const [images,setImages]=useState([]);
   const [isLoading,setIsLoading]=useState(true);
-  const [term,useTerm]=useState('');
+  const [term,setTerm]=useState('');
 
   useEffect(()=>{
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY}&q=${term}&image_type=photo&pretty=true`)
@@ -14,15 +16,18 @@ function App() {
       setIsLoading(false);
     })
     .catch(error=>console.log(error));
-  },[])
+  },[term])
 
   return (
-    <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
+    <div className="container mx-auto items-center">
+      <SearchBar searchText={(text)=>setTerm(text)}/>
+
+      {!isLoading && images.length===0 && <h1 className="text-5xl text-center mx-auto mt-5">No images found with the term <strong>{term}</strong></h1>}
+      {isLoading ? <h1 className="text-5xl text-center mx-auto mt-5">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image=>(
           <ImageCard key={image.id} image={image}></ImageCard>
         ))}
-      </div>
+      </div>}
     </div>
   );
 }
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const ImageCard = ({image}) => {
-    const tags=image.tags.split(',');
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(",");
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-md">
-      <img src={image.webformatURL} alt="" className="w-full"></img>
+    <div className="max-w-sm rounded overflow-hidden shadow-md mt-10">
+      {/* Add a container with fixed dimensions */}
+      <div className="h-64 w-full overflow-hidden">
+        <img
+          src={image.webformatURL}
+          alt=""
+          className="h-full w-full object-cover"
+        />
+      </div>
       <div className="px-6 py-4">
         <div className="font-bold text-green-600 text-xl mb-3">
           Photo by {image.user}
@@ -22,13 +29,17 @@ const ImageCard = ({image}) => {
         </ul>
       </div>
       <div className="px-5 py-3">
-        {tags.map(tag=>(
-            <span key={image.id} className="inline-block bg-blue-200 rounded-full px-3 py-2 text-sm font-semibold text-black-500 mr-3">#{tag}</span>
-
+        {tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block bg-red-400 rounded-full px-3 py-2 text-sm text-center font-semibold text-black-500 mr-3 mb-3"
+          >
+            #{tag}
+          </span>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ImageCard;
